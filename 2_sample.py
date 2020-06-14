6607bfd4ef218e6dee7b3782f88382f69a2c33a8
忠地涼汰 <ryota@eamakku.local>
2020-06-14 20:06:21 +0900

    add new file

---
+++ 2_sample.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def init_network():
+    network={}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([[0.1, 0.2, 0.3]])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([[0.1, 0.2]])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([[0.1, 0.2]])
+    return network
+
+def sigmoid(x):
+    y = 1.0 / (1 + np.exp(-x))
+    return y
+
+def forward(network, x):
+    # layer 1
+    W1 = network['W1']
+    b1 = network['b1']
+    a1 = np.dot(x,W1) + b1
+    z1 = sigmoid(a1)
+
+    #layer2 
+    W2 = network['W2']
+    b2 = network['b2']
+    a2 = np.dot(z1,W2) + b2
+    z2 = sigmoid(a2)
+
+    #layer3
+    W3 = network['W3']
+    b3 = network['b3']
+    y = np.dot(z2,W3) + b3
+    
+    return y
+
+network = init_network()
+x = np.array([1.0,0.5])
+y = forward(network,x)
+print(y) #[0.31682708 0.69627909]
